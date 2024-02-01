b677c24b4c680fa44891d16b69e227f17738228a
angela775 <angela_j2p@hotmail.com>
2024-02-01 17:43:29 -0500

    invalidAPI

--- captura.js
+++ captura.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "3t3af9bf15fb2a631460b188a266fbao";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query={query}&${city}=3t3af9bf15fb2a631460b188a266fbao&units=metric";
+  console.log(apiURL);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
