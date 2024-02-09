bfff9be8239932aab85b98424d6ad3562f27daf5
angela775 <angela_j2p@hotmail.com>
2024-02-09 18:04:54 -0500

    link1api

--- captura.js
+++ captura.js
@@ -1,7 +1,6 @@
 function searchCity(city) {
   let apiKey = "3t3af9bf15fb2a631460b188a266fbao";
-  let apiURL =
-    "https://api.shecodes.io/weather/v1/current?query={query}&${city}=3t3af9bf15fb2a631460b188a266fbao&units=metric";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query={query}&${city}&key=${apiKey}&units=metric`;
   console.log(apiURL);
 }
 function handleSearchSubmit(event) {
