5ff5573687dc4542767bafe78cd5cd98b157af04
angela775 <angela_j2p@hotmail.com>
2024-02-10 18:17:57 -0500

    weathericon

--- captura.js
+++ captura.js
@@ -1,7 +1,12 @@
+function refreshW(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+}
 function searchCity(city) {
   let apiKey = "3t3af9bf15fb2a631460b188a266fbao";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiURL);
+  axios.get(apiURL).then(refreshW);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
